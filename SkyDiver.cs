6b0435961ae06521de5e6cab3269dac1cd6c3907
Spencer Saunders <spencer.saunders45@gmail.com>
2022-05-18 18:26:09 -0600

    working on SkyDiver methods

--- SkyDiver.cs
+++ SkyDiver.cs
@@ -2,9 +2,41 @@
 
 class SkyDiver
 {
-   private string _wordGuess;
+   private int _wrongGuesses;
    public SkyDiver()
    {
+      _wrongGuesses = 0;
+   }
 
+
+
+   /*
+   summary: Shows the value of _wrongGuesses
+   return: (int) the current number of _wrongGuesses
+   */
+   public int GetWrongGuesses()
+   {
+      return _wrongGuesses;
+   }
+
+
+
+   /*
+   summary: Updates the value of _wrongGuesses
+   */
+   public void SetWrongGuesses()
+   {
+      _wrongGuesses++;
+   }
+
+
+
+   /*
+   summary: Updates if the game is over
+   return: (bool) the state of the game
+   */
+   public bool CheckShoot(Word word)
+   {
+      if ()
    }
 }
--- Word.cs
+++ Word.cs
@@ -1,4 +1,4 @@
-// summary: 
+// summary: Holds value of the word, checks guesses, and updates guesses word value
 class Word
 {
    Random _random = new Random();
@@ -6,12 +6,16 @@ class Word
    string[] _wordList = {"hello", "world", "cse210", "jumper"};
    char[] _wordArray;
 
+
+
    // summary: Constructor initializes the word for the game
    public Word()
    {
       _word = _wordList[_random.Next(_wordList.Length + 1)];
       _wordArray = _word.ToCharArray();
    }
+
+
 
    /*
    summary: Checks if a letter if found in the word
@@ -31,13 +35,44 @@ class Word
       return correctGuess;
    }
 
+
+
    /*
    summary: Adds a correct guess into the incomplete word
-   param: (string word) the word being guesses by the jumper
-   return: (string) The guessed word with the added correct letters
+   param: (string word) the word being guesses by the jumper | (string guess) the letter that was guessed
+   return: (string) The guessed word with the added letters
    */
-   public string AddLetter(string word)
+   public string AddLetter(string word, string guess)
    {
-      
+      char[] guessArray = word.ToCharArray();
+      for (int i = 0; i < _wordArray.Length; i++)
+      {
+         if (guess.Equals(_wordArray[i]))
+         {
+            guessArray[i * 2] = Convert.ToChar(guess);
+         }
+      }
+      string updatedWord = new string(guessArray);
+      return updatedWord;
+   }
+
+
+
+   /*
+   summary: Create empty guess word
+   return: (string) dashed string with a space between each dash. 
+   */
+   public string EmptyWord()
+   {
+      string guessWord = String.Empty;
+      for (int i = 0; i < _wordArray.Length; i++){
+         if (i == _wordArray.Length - 1)
+         {
+            guessWord += "_";
+            break;
+         }
+         guessWord += "_ ";
+      }
+      return guessWord;
    }
 }
